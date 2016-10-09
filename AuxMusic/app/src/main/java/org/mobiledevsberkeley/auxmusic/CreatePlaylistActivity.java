7b38ece876b8696d16809a0153958d3b6a53eee5
Young <younglin@berkeley.edu>
2016-10-08 21:25:40 -0700

    got firebase working

--- AuxMusic/app/src/main/java/org/mobiledevsberkeley/auxmusic/CreatePlaylistActivity.java
+++ AuxMusic/app/src/main/java/org/mobiledevsberkeley/auxmusic/CreatePlaylistActivity.java
@@ -9,6 +9,9 @@ import android.widget.EditText;
 import android.widget.Switch;
 import android.widget.TextView;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class CreatePlaylistActivity extends AppCompatActivity {
 
     @Override
@@ -16,9 +19,10 @@ public class CreatePlaylistActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_playlist);
 
-        CheckBox publicPrivate = (CheckBox) findViewById(R.id.publicPrivate);
+        final CheckBox publicPrivate = (CheckBox) findViewById(R.id.publicPrivate);
         final TextView passwordText = (TextView) findViewById(R.id.passwordTextView);
-        final EditText editText = (EditText) findViewById(R.id.passwordEditText);
+        final EditText passwordEditText = (EditText) findViewById(R.id.passwordEditText);
+        final EditText nameText = (EditText) findViewById(R.id.nameTextView);
 
 
         publicPrivate.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
@@ -26,12 +30,31 @@ public class CreatePlaylistActivity extends AppCompatActivity {
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if (isChecked) {
                     passwordText.setVisibility(View.VISIBLE);
-                    editText.setVisibility(View.VISIBLE);
+                    passwordEditText.setVisibility(View.VISIBLE);
                 }
                 else {
                     passwordText.setVisibility(View.GONE);
-                    editText.setVisibility(View.GONE);
+                    passwordEditText.setVisibility(View.GONE);
                 }
+            }
+        });
+
+
+
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DatabaseReference firebase = FirebaseDatabase.getInstance().getReference();
+                DatabaseReference nameRef = firebase.child(getString(R.string.playlistName));
+                DatabaseReference password = firebase.child(getString(R.string.playlistPassword));
+                DatabaseReference passwordProtect = firebase.child(getString(R.string.playlistPasswordProtect));
+                String name = nameText.getText().toString();
+                nameRef.setValue(name);
+                passwordProtect.setValue(publicPrivate.isChecked());
+                if (publicPrivate.isChecked()) {
+                    password.setValue(passwordEditText.getText().toString());
+                }
+
             }
         });
     }
