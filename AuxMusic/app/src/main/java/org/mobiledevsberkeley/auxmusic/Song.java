f5ceb2fea18e687a121d07173cc0d78f652a6015
Wilbur Shi <wilbu@DESKTOP-SBHQII6.(none)>
2016-10-22 13:01:07 -0700

    Add Song class

---
+++ AuxMusic/app/src/main/java/org/mobiledevsberkeley/auxmusic/Song.java
@@ -0,0 +1,47 @@
+package org.mobiledevsberkeley.auxmusic;
+
+/**
+ * Created by wilbu on 10/22/2016.
+ */
+
+public class Song {
+    private String spotifySongURI;
+    private String imageUrl; // potentially save as different variable
+    private String songName;
+    private String artistName;
+    private String albumName;
+    private double trackLength;
+
+    public Song(String spotifySongURI) {
+        this.spotifySongURI = spotifySongURI;
+        queryAndInitialize();
+    }
+
+    private void queryAndInitialize() {
+        // query from Spotify the information about the track given the spotifySongUri
+    }
+
+    public String getSpotifySongURI() {
+        return spotifySongURI;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public String getSongName() {
+        return songName;
+    }
+
+    public String getArtistName() {
+        return artistName;
+    }
+
+    public String getAlbumName() {
+        return albumName;
+    }
+
+    public double getTrackLength() {
+        return trackLength;
+    }
+}
