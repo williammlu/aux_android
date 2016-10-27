92e5ba496313f5dc0822b190ecc81aa90f0af2a4
Wilbur Shi <wilbu@DESKTOP-SBHQII6.(none)>
2016-10-26 22:17:50 -0700

    Standardize variable names from backend to Java for safe conversion

--- AuxMusic/app/src/main/java/org/mobiledevsberkeley/auxmusic/Playlist.java
+++ AuxMusic/app/src/main/java/org/mobiledevsberkeley/auxmusic/Playlist.java
@@ -6,42 +6,58 @@ import java.util.*;
  */
 
 public class Playlist {
-    private List<String> userIDList;            // contains UIDs from users. we could then convert this to a List<User>, but this may be slow
+    private List<String> userDeviceIDList;            // contains UIDs from users. we could then convert this to a List<User>, but this may be slow
     private List<String> spotifySongIDList;     // contains spotify track URIs, we could then convert this to a List<Song>, but ^^
     private String playlistName;
     private String password;
-    private List<Song> songList; // or Queue<Song> for our own queue
-    private List<User> participantList;
-    private String playlistKey; // Firebase hash to have easy access to itself in Firebase
-    // we can have singleton class reference here for database reference, etc.
-    // other playlist settings like tracklocation, explicitness for filtering (future option)
-    // WE NEED MORE STUFF HERE BUT I'M TIRED AF SO YEAH
+    private String hostDeviceID;
+    private String currentSongID;
+    private Boolean isPlaying;
+    private long currentSongTime;
 
     public Playlist() {
         // initialize lists so that they're not null. not sure if this is necessary though. this null constructor
         // might be necessary when converting from Firebase stuff to a Java object
     }
 
-    public Playlist(List<String> userIDList, List<String> spotifySongIDList, String playlistName, String password) {
-        this.userIDList = userIDList;
+    public Playlist(List<String> userDeviceIDList, List<String> spotifySongIDList, String playlistName,
+                    String password, String hostDeviceID, String currentSongID, Boolean isPlaying, long currentSongTime) {
+        this.userDeviceIDList = userDeviceIDList;
         this.spotifySongIDList = spotifySongIDList;
         this.playlistName = playlistName;
         this.password = password;
-    }
-
-    public String getplaylistKey() {
-        return playlistKey;
+        this.hostDeviceID = hostDeviceID;
+        this.currentSongID = currentSongID;
+        this.isPlaying = isPlaying;
+        this.currentSongTime = currentSongTime;
     }
 
     public void addSong(Song song) {
-        songList.add(song);
-        spotifySongIDList.add(song.getSpotifySongURI());
-        // no need to update database because the singleton should add to database before calling this.
+        spotifySongIDList.add(song.getSongURI());
     }
 
     public void addUser(User user) {
-        participantList.add(user);
-        userIDList.add(user.getUID());
+        userDeviceIDList.add(user.getUID());
+    }
+
+    public List<String> getUserDeviceIDList() {
+        return userDeviceIDList;
+    }
+
+    public String getHostDeviceID() {
+        return hostDeviceID;
+    }
+
+    public String getCurrentSongID() {
+        return currentSongID;
+    }
+
+    public Boolean getPlaying() {
+        return isPlaying;
+    }
+
+    public long getCurrentSongTime() {
+        return currentSongTime;
     }
 
     public List<String> getSpotifySongIDList() {
@@ -52,8 +68,8 @@ public class Playlist {
         return password;
     }
 
-    public List<String> getUserIDList() {
-        return userIDList;
+    public List<String> getuserDeviceIDList() {
+        return userDeviceIDList;
     }
 
     public String getPlaylistName() {
